383f9c6c87273dac40473325bc1c369ab46faca1
Javier Solis <javiersol2000@outlook.com>
2022-01-16 21:43:18 -0600

    Elementos de pantalla para reservar cita

--- app/src/main/java/com/jssdeveloper/mydoctor/MenuActivity.kt
+++ app/src/main/java/com/jssdeveloper/mydoctor/MenuActivity.kt
@@ -1,11 +1,18 @@
 package com.jssdeveloper.mydoctor
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        btnCreateAppointment.setOnClickListener{
+            val intent = Intent(this, CreateAppointmentActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
